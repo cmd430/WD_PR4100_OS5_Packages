6c660c12ea9bfc943e53ac3f55846d37cff12980
cmd430 <cmd430@gmail.com>
2023-02-17 06:30:14 +1300

    correct base to use APP_NAME for symlink for web

--- wdpk/_base/init.sh
+++ wdpk/_base/init.sh
@@ -4,4 +4,4 @@ APP_NAME="$(basename $APP_PATH)"
 
 echo $0 $* | tee -a "/tmp/${APP_NAME}.log"
 
-ln -s "$APP_PATH" "/var/www/${APP_PATH}" 2>/dev/null
+ln -s "$APP_PATH" "/var/www/${APP_NAME}" 2>/dev/null
