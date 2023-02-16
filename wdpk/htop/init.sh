980b06e4195f6ab6968b6c9f9bd89886740f19f1
cmd430 <cmd430@gmail.com>
2023-02-17 06:33:30 +1300

    adjust install/init scripts

--- wdpk/htop/init.sh
+++ wdpk/htop/init.sh
@@ -5,11 +5,6 @@ APP_NAME="$(basename $APP_PATH)"
 echo $0 $* | tee -a "/tmp/${APP_NAME}.log"
 
 ln -s "$APP_PATH" "/var/www/${APP_NAME}" 2>/dev/null
-
-chmod 755 "${APP_PATH}/bin/htop"
-chmod -R 755 "${APP_PATH}/terminfo"
-chown -R root:root "${APP_PATH}/terminfo"
-
 ln -s "${APP_PATH}/bin/htop" "/bin/htop"
 ln -sfnT "${APP_PATH}/terminfo" "/usr/share/terminfo"
 
--- wdpk/htop/install.sh
+++ wdpk/htop/install.sh
@@ -8,3 +8,8 @@ INSTALL_SRC="$1"
 INSTALL_DEST="$2"
 
 mv "$INSTALL_SRC" "$INSTALL_DEST"
+
+chmod -R 755 "${APP_PATH}/bin"
+chmod -R 755 "${APP_PATH}/terminfo"
+chown -R root:root "${APP_PATH}/bin"
+chown -R root:root "${APP_PATH}/terminfo"
